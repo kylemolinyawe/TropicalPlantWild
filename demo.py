f917c998e7380b2a7b54025230e5c2a9d376fb54
Kyle Molinyawe <kylelorenzo.molinyawe.cics@ust.edu.ph>
2025-11-19 16:38:32 +0800

    Uncommenting demo.py

--- demo.py
+++ demo.py
@@ -25,18 +25,18 @@ example_index = 374
 # ---
 
 # Creating the master index as a reference for every sample in the dataset
-# utils.generate_master_index(img_demo_dir, demo_dir)
+utils.generate_master_index(img_demo_dir, demo_dir)
 train_set, val_set, test_set = utils.load_master_index(demo_dir)
 
-# print("")
-# preprocessing.extract_rois_fasterrcnn(demo_dir, 'train', train_set, num_rois)
-# train_tensors_rois = utils.get_tensor_rois_file_paths(demo_dir, 'train')
-# utils.count_tensors_rois_shape(train_tensors_rois)
+print("")
+preprocessing.extract_rois_fasterrcnn(demo_dir, 'train', train_set, num_rois)
+train_tensors_rois = utils.get_tensor_rois_file_paths(demo_dir, 'train')
+utils.count_tensors_rois_shape(train_tensors_rois)
 
-# print("")
-# preprocessing.extract_rois_fasterrcnn(demo_dir, 'val', val_set, num_rois)
-# val_tensors_rois = utils.get_tensor_rois_file_paths(demo_dir, 'val')
-# utils.count_tensors_rois_shape(val_tensors_rois)
+print("")
+preprocessing.extract_rois_fasterrcnn(demo_dir, 'val', val_set, num_rois)
+val_tensors_rois = utils.get_tensor_rois_file_paths(demo_dir, 'val')
+utils.count_tensors_rois_shape(val_tensors_rois)
 
 print("")
 preprocessing.extract_rois_fasterrcnn(demo_dir, 'test', test_set, num_rois)
@@ -44,23 +44,23 @@ test_tensors_rois = utils.get_tensor_rois_file_paths(demo_dir, 'test')
 utils.count_tensors_rois_shape(test_tensors_rois)
 
 
-# # Visualizing the ROIs per sample
-# for item in train_set:
-#     vis.plot_rois_on_image(demo_dir, train_set, item['id'])
+# Visualizing the ROIs per sample
+for item in train_set:
+    vis.plot_rois_on_image(demo_dir, train_set, item['id'])
 
 
 # 2 Feature Vectors
 # ---
 
-# print("")
-# preprocessing.extract_feature_vectors(demo_dir, 'train', train_tensors_rois, train_set)
-# train_tensors_rois_features = utils.get_tensor_rois_features_file_paths(demo_dir, 'train')
-# utils.count_tensors_rois_shape(train_tensors_rois_features)
+print("")
+preprocessing.extract_feature_vectors(demo_dir, 'train', train_tensors_rois, train_set)
+train_tensors_rois_features = utils.get_tensor_rois_features_file_paths(demo_dir, 'train')
+utils.count_tensors_rois_shape(train_tensors_rois_features)
 
-# print("")
-# preprocessing.extract_feature_vectors(demo_dir, 'val', val_tensors_rois, val_set)
-# val_tensors_rois_features = utils.get_tensor_rois_features_file_paths(demo_dir, 'val')
-# utils.count_tensors_rois_shape(val_tensors_rois_features)
+print("")
+preprocessing.extract_feature_vectors(demo_dir, 'val', val_tensors_rois, val_set)
+val_tensors_rois_features = utils.get_tensor_rois_features_file_paths(demo_dir, 'val')
+utils.count_tensors_rois_shape(val_tensors_rois_features)
 
 print("")
 preprocessing.extract_feature_vectors(demo_dir, 'test', test_tensors_rois, test_set)
@@ -72,54 +72,30 @@ utils.count_tensors_rois_shape(test_tensors_rois_features)
 # # 3 Visual Prototypes
 # # ---
 
-# preprocessing.construct_visual_prototypes(demo_dir, train_tensors_rois_features, train_set)
-# utils.inspect_visual_prototypes(demo_dir)
+preprocessing.construct_visual_prototypes(demo_dir, train_tensors_rois_features, train_set)
+utils.inspect_visual_prototypes(demo_dir)
 
 # # 4 Model Training (VisualBERT)
 # #---
 
-# # Text prompts
-# print("")
-# text_prompts = utils.load_text_prompts()
-# print(text_prompts.keys())
-# model.tokenize_text_prompts(demo_dir, text_prompts)
-# utils.inspect_tokenized_prompts(demo_dir)
+# Text prompts
+print("")
+text_prompts = utils.load_text_prompts()
+print(text_prompts.keys())
+model.tokenize_text_prompts(demo_dir, text_prompts)
+utils.inspect_tokenized_prompts(demo_dir)
 
-# # Labels
-# print("")
-# labels = utils.get_labels(demo_dir)
-# labels_int = model.map_labels_to_int(labels)
+# Labels
+print("")
+labels = utils.get_labels(demo_dir)
+labels_int = model.map_labels_to_int(labels)
 
-# # Preparing VisualBERT input
-# print("")
-# train_visualbert_input = model.prepare_visualbert_input(demo_dir, train_set, train_tensors_rois_features, labels_int, num_rois)
-# val_visualbert_input = model.prepare_visualbert_input(demo_dir, val_set, val_tensors_rois_features, labels_int, num_rois)
-
-
-# # Training VisualBERT
-# print("")
-# model.train_visualbert(models_dir, train_visualbert_input, val_input=val_visualbert_input)
+# Preparing VisualBERT input
+print("")
+train_visualbert_input = model.prepare_visualbert_input(demo_dir, train_set, train_tensors_rois_features, labels_int, num_rois)
+val_visualbert_input = model.prepare_visualbert_input(demo_dir, val_set, val_tensors_rois_features, labels_int, num_rois)
 
 
-# # # 5 Evaluation
-# # #---
-# # # ROIs
-# # preprocessing.extract_rois(val_paths, folder_filepath=val_demo_dir)
-# # preprocessing.extract_rois(test_paths, folder_filepath=test_demo_dir)
-
-# # # Building the ROI file path list
-# # val_roi_paths = utils.get_file_paths(val_rois_dir)
-# # test_roi_paths = utils.get_file_paths(test_rois_dir)
-
-# # # Sorting the list wherein the index matches the file name
-# # val_roi_paths = utils.reorder_list_by_filename(val_roi_paths)
-# # test_roi_paths = utils.reorder_list_by_filename(test_roi_paths)
-
-# # # Extracting feature vectors from ROIs
-# # preprocessing.extract_roi_features(val_roi_paths, folder_filepath=val_demo_dir)
-# # preprocessing.extract_roi_features(test_roi_paths, folder_filepath=test_demo_dir)
-
-# val_roi_feature_paths = utils.get_file_paths(val_rois_features_dir)
-# test_roi_feature_paths = utils.get_file_paths(test_rois_features_dir)
-
-# evaluate.evaluate_visualbert('demo.pth', test_roi_feature_paths, labels, prompts_list, num_classes)
+# Training VisualBERT
+print("")
+model.train_visualbert(models_dir, train_visualbert_input, val_input=val_visualbert_input)
